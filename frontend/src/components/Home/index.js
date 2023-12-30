ef289e6b49567866591757e992c39a7198065811
Mehmet Semih BABACAN <55881663+koltukutsu@users.noreply.github.com>
2023-12-31 01:02:07 +0300

    Add: added take offers sectiong

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import {
   Typography,
+  Box,
   Button,
   Container,
   Select,
+  TextField,
   Grid,
   MenuItem,
   TableContainer,
@@ -19,6 +21,9 @@ import { UserInformation } from "../../features/user/UserInformation";
 const Home = () => {
   const [selectedOrganization, setSelectedOrganization] = useState({});
   const [selectedCompany, setSelectedCompany] = useState({});
+  const [selectedSeason, setSelectedSeason] = useState({});
+  const [selectedNumberOfPeople, setSelectedNumberOfPeople] = useState(0);
+
   const [organizations, setOrganizations] = useState([]);
   const [companies, setCompanies] = useState([]);
   const [offers, setOffers] = useState([]);
@@ -32,6 +37,21 @@ const Home = () => {
     console.log("Here is the selected Company: ", event.target.value)
     setSelectedCompany(event.target.value);
   };
+
+  const handleSelectedSeason = (event) => {
+    console.log("Here is the selected Season: ", event.target.value)
+    setSelectedSeason(event.target.value);
+  };
+
+  const handleSelectedNumberOfPeople = (event) => {
+    console.log("Here is the selected Number of People: ", event.target.value)
+    setSelectedNumberOfPeople(event.target.value);
+  };
+
+  const handleUserSpecialChoices = () => {
+    console.log("Here is the selected Season: ", selectedSeason)
+    console.log("Here is the selected Number of People: ", selectedNumberOfPeople)
+  }
 
   useEffect(() => {
     document.title = "Parti Dünyası";
@@ -226,9 +246,9 @@ const Home = () => {
               <TableCell>{offer.id}</TableCell>
               <TableCell>{companies.find(
                 (company) => company.id === offer.comp_id
-              ).comp_name}</TableCell>
+              )?.comp_name}</TableCell>
               <TableCell>
-                {organizations.find((organization) => organization.id === offer.org_type).org_name}
+                {organizations.find((organization) => organization.id === offer.org_type)?.org_name}
               </TableCell>
               <TableCell>{offer.accepted}</TableCell>
               <TableCell>{offer.max_guest_count}</TableCell>
@@ -270,6 +290,56 @@ const Home = () => {
     );
   };
 
+  const UserSpecialChoices = () => {
+    const handleFormSubmit = (e) => {
+      e.preventDefault();
+
+      handleUserSpecialChoices();
+    };
+
+    return (
+      <Box component="form" onSubmit={handleFormSubmit} noValidate sx={{ mt: 1 }}>
+        <Select
+          required
+          fullWidth
+          margin="normal"
+          id="season"
+          name="season"
+          label="Mevsim Seçiniz"
+          value={selectedSeason}
+          onChange={handleSelectedSeason}
+          sx={{ marginTop: 2 }}
+        >
+          {companies.map((company) => (
+            <MenuItem key={company.id} value={company}>
+              {company.comp_name}
+            </MenuItem>
+          ))}
+        </Select>
+
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          type="number"
+          id="amountOfPeople"
+          label="İnsan Sayısı Giriniz"
+          name="amountOfPeople"
+          autoFocus
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Teklif Al
+        </Button>
+      </Box>
+    );
+  };
+
+
   return (
     <Container className="pt-10">
       <UserInformation />
@@ -279,6 +349,9 @@ const Home = () => {
         </Grid>
         <Grid item xs={4}>
           <ChooseCompanyType />
+        </Grid>
+        <Grid item xs={4}>
+          < UserSpecialChoices />
         </Grid>
         <Grid item xs={12}>
           <AllOffersForTheOrganization />
